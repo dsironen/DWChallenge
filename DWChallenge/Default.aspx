68e1352610dfbdd864d02019485c554ad2fb322f
Daniel Sironen <dsironen@hotmail.com>
2016-01-21 19:44:58 -0500

    .

---
+++ DWChallenge/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DWChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        First Value:
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        <br />
+        Second Value:
+        <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ DWChallenge/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DWChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            int firstValue = int.Parse(firstTextBox.Text);
+            int secondValue = int.Parse(secondTextBox.Text);
+
+            int totalValue = 0;
+
+            while (totalValue < 100)
+            {
+                totalValue += firstValue + secondValue;
+                resultLabel.Text += "<br />" + totalValue.ToString();
+            }
+
+            resultLabel.Text += "<br />" + String.Format("Final Result {0}", totalValue.ToString());
+        }
+    }
+}
